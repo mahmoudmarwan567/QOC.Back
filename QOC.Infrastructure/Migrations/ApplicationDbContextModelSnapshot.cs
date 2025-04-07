6474aed5dc1f3460da20a95eab0033bb4626c56e
Mahmoud Marwan <mahmoud.marwan567@gmail.com>
2025-04-07 21:36:05 +0200

    gdf

--- QOC.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ QOC.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -336,49 +336,6 @@ namespace QOC.Infrastructure.Migrations
                     b.ToTable("CompanyPhones");
                 });
 
-            modelBuilder.Entity("QOC.Domain.Entities.Project.Project", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<string>("ProjectDescription")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("ProjectName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Projects");
-                });
-
-            modelBuilder.Entity("QOC.Domain.Entities.Project.ProjectImage", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<string>("ImagePath")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("ProjectId")
-                        .HasColumnType("int");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("ProjectId");
-
-                    b.ToTable("ProjectImages");
-                });
-
             modelBuilder.Entity("QOC.Domain.Entities.Slider", b =>
                 {
                     b.Property<int>("Id")
