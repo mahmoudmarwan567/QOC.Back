ff121e6772d68d6b51bb0fcba41310f31974a633
Mahmoud Marwan <mahmoud.marwan567@gmail.com>
2025-04-20 01:00:27 +0200

    helper

---
+++ QOC.Infrastructure/Helpers/CultureHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace QOC.Infrastructure.Helpers
+{
+    public static class CultureHelper
+    {
+        public static bool IsArabic() =>
+            CultureInfo.CurrentCulture.TwoLetterISOLanguageName == "ar";
+    }
+}
