d84ccc5167fc832efa5e05d93e5b0f4a62abed4b
Amr Raafat <amrraafat>
2025-06-10 23:40:24 +0300

    a

---
+++ QOC.Api/Helpers/EnforceAuthorizationMiddleware.cs
@@ -0,0 +1,37 @@
+﻿namespace QOC.Api.Helpers
+{
+    public class EnforceAuthorizationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public EnforceAuthorizationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var path = context.Request.Path.Value?.ToLower();
+
+            // Skip authorization for these specific paths
+            var skipAuthPaths = new[]
+            {
+            "/api/auth/login",
+            "/api/auth/register"
+        };
+
+            if ((HttpMethods.IsPost(context.Request.Method) ||
+                 HttpMethods.IsPut(context.Request.Method) ||
+                 HttpMethods.IsDelete(context.Request.Method))
+                && !context.User.Identity.IsAuthenticated
+                && !skipAuthPaths.Contains(path))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
